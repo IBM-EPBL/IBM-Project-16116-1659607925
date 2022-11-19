bea504a49e74c7e2e3054aa7fb5194fda8836835
jerome1232002 <111100583+jerome1232002@users.noreply.github.com>
2022-11-19 20:23:32 +0530

    Add files via upload

---
+++ Final Delivarables/templates/data.php
@@ -0,0 +1,14 @@
+<?php
+    $n=$_POST['Name'];
+    $p=$_POST['phonenumber'];
+    $e=$_POST['Email'];
+$con=mysqli_connect('localhost','root',"",'data');
+$sql="INSERT INTO details ('Name','Phonenumber','Email')VALUES('$n','$c','$e')";
+$r=$mysqli_query($con,$sql);
+
+if($r)
+{
+    echo "Details inserted";
+}
+?>
+
